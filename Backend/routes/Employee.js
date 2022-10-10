6e61f37d262082bd9a5069d7e0a230aff1d463ab
AnushaO <97550806+AnushaO@users.noreply.github.com>
2022-10-10 23:41:19 +0530

    Update Employee.js

--- Backend/routes/Employee.js
+++ Backend/routes/Employee.js
@@ -79,7 +79,7 @@ router.delete('/Employee/delete/:id',(req,res) =>{
 
 
 
-//router.delete('/Employee/delete/:id',(req,res) =>{
+router.delete('/Employee/delete/:id',(req,res) =>{
     Employee.findByIdAndRemove(req.params.id).exec((err,deleteEmployee) =>{
 
         if(err) return res.status(400).json({
