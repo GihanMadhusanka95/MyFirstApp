1f8eb24fd0cc1d87ffc95c83ebb4fc4af4be8861
gihan <it20648718@my.sliit.lk>
2022-10-11 10:17:31 +0530

    Update NoticeReport.js

--- myfirstapp/src/components/NoticeReport.js
+++ myfirstapp/src/components/NoticeReport.js
@@ -41,7 +41,7 @@ export default function NoticeReport() {
            
         <div className="l2"> 
         
-        <h4> notice is create</h4>
+        <h4> notice is create on</h4>
         
         </div>
         
