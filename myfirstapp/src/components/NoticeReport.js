345178ca218190f52a717c94374b79972b49620b
gihan <it20648718@my.sliit.lk>
2022-10-11 11:14:26 +0530

    Update NoticeReport.js

--- myfirstapp/src/components/NoticeReport.js
+++ myfirstapp/src/components/NoticeReport.js
@@ -41,7 +41,8 @@ export default function NoticeReport() {
            
         <div className="l2"> 
         
-        <h4> notice </h4>
+        <h4> We are going to make a full functional web application for train ticket booking. We hope to make this application make things easier for the customer. There are 4 main functions in this app, they are bookings management, customer management, employee management, and notice and feedback management. We hope to use MERN STACK to develop our project, MERN stack is a JavaScript software stack for building dynamic web applications. 
+             Each functions had full crud operations.</h4>
         
         </div>
         
