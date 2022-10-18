2c327f08a578a7691b01d6551156d549ff9d74bd
bino1999 <101051395+bino1999@users.noreply.github.com>
2022-10-18 10:44:30 +0530

    rosi
    2.2

--- myfirstapp/src/components/EditImage.js
+++ myfirstapp/src/components/EditImage.js
@@ -1,7 +1,224 @@
 import React from 'react'
+import Header from './Header'
+import './css files/Galery.css';
+
 
 export default function EditImage() {
   return (
-    <div>EditImage</div>
+    
+
+    <div>
+
+     <div>
+        <Header/>
+     </div>
+      
+      <div className="row">
+        <div className="col-4">
+           
+            <div className="card">
+                <div className="card-body">
+                   
+                <img src="./images/1.jpg"  alt="" />
+
+                </div>
+
+                <div className="card-footer">
+
+                  
+                    <button className='btn btn-danger jop'>Delete</button>          
+          
+                </div>
+            </div>
+
+        </div>
+
+        <div className="col-4">
+           
+            <div className="card">
+                <div className="card-body">
+
+                <img src="./images/2.jpg"  alt="" />
+
+                </div>
+             
+                <div className="card-footer">
+
+
+ <button className='btn btn-danger jop'>Delete</button>          
+
+</div>
+
+
+            </div>
+
+        </div>
+
+        <div className="col-4">
+           
+            <div className="card">
+                <div className="card-body">
+               
+                <img src="./images/3.webp"  alt="" />
+                    
+                </div>
+
+                <div className="card-footer">
+
+              
+             <button className='btn btn-danger jop'>Delete</button>          
+
+              </div>
+
+
+            </div>
+
+        </div>
+      </div>
+    
+
+
+      <div className="row">
+        <div className="col-4">
+           
+            <div className="card">
+                <div className="card-body">
+                 
+                <img src="./images/4.jpg"  alt="" />
+
+                </div>
+
+                <div className="card-footer">
+
+
+ <button className='btn btn-danger jop'>Delete</button>          
+
+</div>
+
+
+            </div>
+
+        </div>
+
+        <div className="col-4">
+           
+            <div className="card">
+                <div className="card-body">
+                
+                <img src="./images/1.jpg"  alt="" />
+                    
+                </div>
+
+                <div className="card-footer">
+
+
+ <button className='btn btn-danger jop'>Delete</button>          
+
+</div>
+
+            </div>
+
+        </div>
+
+        <div className="col-4">
+           
+            <div className="card">
+                <div className="card-body">
+                 
+                <img src="./images/1.jpg"  alt="" />
+                    
+                </div>
+
+                <div className="card-footer">
+
+
+ <button className='btn btn-danger jop'>Delete</button>          
+
+</div>
+
+
+            </div>
+
+        </div>
+      </div>
+
+
+      <div className="row">
+        <div className="col-4">
+           
+            <div className="card">
+                <div className="card-body">
+                     
+
+                <img src="./images/1.jpg"  alt="" />
+
+                </div>
+                   
+                <div className="card-footer">
+
+
+ <button className='btn btn-danger jop'>Delete</button>          
+
+</div>
+
+
+            </div>
+
+        </div>
+
+        <div className="col-4">
+           
+            <div className="card">
+                <div className="card-body">
+                   
+                <img src="./images/4.jpg"  alt="" />
+                    
+                </div>
+
+                <div className="card-footer">
+
+       
+                 <button className='btn btn-danger jop'>Delete</button>          
+
+</div>
+
+            </div>
+
+        </div>
+
+        <div className="col-4">
+           
+            <div className="card">
+                <div className="card-body">
+              
+                <img src="./images/4.jpg"  alt="" />
+                    
+                </div>
+
+                <div className="card-footer">
+
+<button className='btn btn-success jop'>Add</button>
+ <button className='btn btn-danger jop'>Change</button>          
+
+</div>
+
+
+            </div>
+
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
   )
 }
